fa58f3d664ec7bbc0fe6a897074b1ff446ca9469
aldarris <dovs@hwsrv-666133.hostwindsdns.com>
2020-01-27 22:56:14 +0000

    Merged

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 // Main game objects
 var App = require("http").createServer();
-var DefaultOrigins = "http://www.bondageprojects.com:* https://www.bondageprojects.com:* http://bondageprojects.com:* https://bondageprojects.com:* http://www.bondageprojects.elementfx.com:* https://www.bondageprojects.elementfx.com:* http://bondageprojects.elementfx.com:* https://bondageprojects.elementfx.com:* http://127.0.0.1:* http://localhost:*";
+var DefaultOrigins = "http://hwsrv-666133.hostwindsdns.com:* http://www.bondageprojects.com:* https://www.bondageprojects.com:* http://bondageprojects.com:* https://bondageprojects.com:* http://www.bondageprojects.elementfx.com:* https://www.bondageprojects.elementfx.com:* http://bondageprojects.elementfx.com:* https://bondageprojects.elementfx.com:* http://127.0.0.1:* http://localhost:*";
 var IO = require("socket.io")(App, { origins: process.env.ORIGINS || DefaultOrigins, maxHttpBufferSize: 200000 } );
 var BCrypt = require("bcrypt");
 var Account = [];
